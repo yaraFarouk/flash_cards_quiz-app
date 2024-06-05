1586e75db32fdca9a0ac500c89ad195bdfeda466
yaraFarouk <yaraFarouk960@gmail.com>
2024-06-05 22:23:54 +0300

    provide add note cubit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flashcard_quize_app/constants.dart';
+import 'package:flashcard_quize_app/cubits/cubit/add_cards_cubit.dart';
 import 'package:flashcard_quize_app/models/note_model.dart';
 import 'package:flashcard_quize_app/views/cards_view.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   await Hive.initFlutter();
@@ -16,13 +18,20 @@ class FlashCardsQuizApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AddCardsCubit(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const CardsView(),
       ),
-      home: const CardsView(),
     );
   }
 }
