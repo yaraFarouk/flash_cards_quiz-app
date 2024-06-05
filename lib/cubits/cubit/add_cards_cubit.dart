2e9b12e252c40ffbb23ad511fda7b794038defbe
yaraFarouk <yaraFarouk960@gmail.com>
2024-06-05 22:12:54 +0300

    add addcard method to add card cubit

--- lib/cubits/cubit/add_cards_cubit.dart
+++ lib/cubits/cubit/add_cards_cubit.dart
@@ -1,8 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:flashcard_quize_app/constants.dart';
+import 'package:flashcard_quize_app/models/note_model.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 
 part 'add_cards_state.dart';
 
 class AddCardsCubit extends Cubit<AddCardsState> {
   AddCardsCubit() : super(AddCardsInitial());
+  addCard(CardModel card) async {
+    emit(AddCardLoading());
+    try {
+      var cardBox = Hive.box<CardModel>(kCardsBox);
+      emit(AddCardSuccess());
+      await cardBox.add(card);
+    } catch (e) {
+      AddCardFailure(errMessage: e.toString());
+    }
+  }
 }
